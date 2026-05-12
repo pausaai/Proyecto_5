9d35f2d3cacdab1b528d1b7909a6ef34c7b771ab
pausaai <pau.sala.aizpun@estudiantat.upc.edu>
2026-05-12 18:13:46 +0200

    implement SetGates function
    Refactor class definitions and add SetGates function.

--- LEBL.py
+++ LEBL.py
@@ -17,3 +17,22 @@ class BarcelonaAP:
     def __init__(self):
         self.code = ""
         self.terminals = []
+def SetGates(area, init_gate, end_gate, prefix):
+    if init_gate > end_gate:
+        return(-1)
+    schengenAreas = ["a", "b", "c", "m", "r", "s", "u"]
+    d = 0
+    while d != len(schengenAreas):
+        if area.name == schengenAreas[d]:
+            area.type = True
+            d = d + 1
+        else:
+            d = d + 1
+    i = init_gate
+    while i <= end_gate:
+        g = Gate()
+        g.name = prefix + area.name + str(i)
+        g.free = True
+        area.gates.append(g)
+        i += 1
+    return area
